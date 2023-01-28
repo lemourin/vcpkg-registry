7f08f6c69771f346d0f4dc5f2bf0f9198d429a51
Paweł Wegner <pawel.wegner95@gmail.com>
2023-01-29 00:02:27 +0100

    coro-http: updated.

--- ports/coro-http/portfile.cmake
+++ ports/coro-http/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO lemourin/coro-http
-  REF 65bc1a9df8f551e7f62ca8d1e6d47a79df328459
-  SHA512 d5332cc72fa487f5378827f0fbca88295fe80fbb005283ce3ae7338d58948a8a10136bbacb98d5f73ea2d84fa6f3238c0b39166f37930f7068829aa1b07f528e
+  REF eb74bad3a949c8d90fade1dfe37e00c2833f915a
+  SHA512 05985a4d749cceb150e5cda425dc22887e7d42b460f9894715ee4cb084aa38192801ad5d32a93dfe816b0c693e1f5ceda7c61570c8d19bcac5e99f9658c9b73c
   HEAD_REF master
 )
 
@@ -11,7 +11,7 @@ vcpkg_configure_cmake(
   PREFER_NINJA
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/coro-http)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/coro-http)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
