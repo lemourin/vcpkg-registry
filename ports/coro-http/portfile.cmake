0474242b72be49ef42b59b892f392071643c8b59
Paweł Wegner <pawel.wegner95@gmail.com>
2023-01-28 22:39:08 +0100

    coro-http: updated.

--- ports/coro-http/portfile.cmake
+++ ports/coro-http/portfile.cmake
@@ -1,14 +1,14 @@
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO lemourin/coro-http
-	REF 551bde93fc1458ac7fe5e082fe8fb2e921c3a3b8
-  SHA512 17c91ba34fd5af973cd8969df95b178e0745d8aa13cc3c9a2ae54a66cf5163c0049f0cd862a240b319ec7dcdefca3244f370dad387ac241f53d0a3332708723b
-	HEAD_REF master
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO lemourin/coro-http
+  REF 65bc1a9df8f551e7f62ca8d1e6d47a79df328459
+  SHA512 d5332cc72fa487f5378827f0fbca88295fe80fbb005283ce3ae7338d58948a8a10136bbacb98d5f73ea2d84fa6f3238c0b39166f37930f7068829aa1b07f528e
+  HEAD_REF master
 )
 
 vcpkg_configure_cmake(
-	SOURCE_PATH "${SOURCE_PATH}"
-	PREFER_NINJA
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/coro-http)
