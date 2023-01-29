1a722ff4e2a246c3c6742f0fd87b50587c7a759e
Paweł Wegner <pawel.wegner95@gmail.com>
2023-01-29 22:01:49 +0100

    coro-cloudstorage: updated.

--- ports/coro-cloudstorage/portfile.cmake
+++ ports/coro-cloudstorage/portfile.cmake
@@ -1,14 +1,16 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO lemourin/coro-cloudstorage
-  REF 7cdb209b782377f237c3d0244eeeb1c4f3df9f9f
-  SHA512 22dc5369055dbf5daf29fb218e8ce50696f48e0d1b0c5334f1ca85eddded274c6bbb4997b20925fbf426176a834832d5eec5c00bab0d1c0a3c33d238c7980ef4
+  REF dc153ca023555a7f7b59948b7fbaab54ada59a02
+  SHA512 11709b2cac4912552a0df7ee73c61e86d4360830a83f2338eda35000ba1de469a9fd71e088211a0bc7e3fc1f829e82985c31fe60ab88d89b79484183f6c7c701
   HEAD_REF master
 )
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
+  OPTIONS
+    -DBUILD_EXAMPLES=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/coro-cloudstorage)
