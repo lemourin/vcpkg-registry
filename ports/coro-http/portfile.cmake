7f7d070df820bb3d2cb3a28c26d0b151f18beb8d
Paweł Wegner <pawel.wegner95@gmail.com>
2023-03-26 11:04:02 +0200

    coro-http: update.

--- ports/coro-http/portfile.cmake
+++ ports/coro-http/portfile.cmake
@@ -1,17 +1,20 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO lemourin/coro-http
-  REF 6f496d70615fdc3aa6ea48ded27a62d563f46ab5
-  SHA512 6fdc1b439b53b1fcae1ef35b56690fd6dd0e3aaddfb9ed0f2a360162e80ab9489167b1b99d6b9085c665740fb9d5fda7c60acf9f324580df90d4f0e0b6d40da8
+  REF 806be29b4fd7ac0f702ee64004f7c5e8603e9cbd
+  SHA512 7db5c16df4b2a792deae7682bb1a46d7ec530d239566950134f1bd9036a5b0d56f036f9b58fabf0da82e40ca398da4123730042584edec6d5a6bfc53e95a9a1f
   HEAD_REF master
 )
+
+set(OPTIONS "-DBUILD_EXAMPLES=OFF")
+if("stacktrace" IN_LIST FEATURES)
+  list(APPEND OPTIONS "-DWITH_STACKTRACE=ON")
+endif()
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
-  OPTIONS
-    -DBUILD_EXAMPLES=OFF
-    -DBOOST_STACKTRACE=OFF
+  OPTIONS ${OPTIONS}
 )
 vcpkg_cmake_install(DISABLE_PARALLEL)
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/coro-http)
