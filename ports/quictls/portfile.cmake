8ad2de22d5062199efc59bb6c80346cf8b8bc6b2
Paweł Wegner <pawel.wegner95@gmail.com>
2023-11-17 00:25:19 +0100

    quictls: copy openssl port.

--- ports/quictls/portfile.cmake
+++ ports/quictls/portfile.cmake
@@ -1,192 +1,75 @@
-if(EXISTS ${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h)
-    message(FATAL_ERROR "Can't build '${PORT}' if another SSL library is installed. Please remove existing one and try install '${PORT}' again if you need it.")
+if(EXISTS "${CURRENT_INSTALLED_DIR}/share/libressl/copyright"
+    OR EXISTS "${CURRENT_INSTALLED_DIR}/share/boringssl/copyright")
+    message(FATAL_ERROR "Can't build openssl if libressl/boringssl is installed. Please remove libressl/boringssl, and try install openssl again if you need it.")
 endif()
+
+if(VCPKG_TARGET_IS_EMSCRIPTEN)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+if (NOT "${VERSION}" MATCHES [[^([0-9]+)\.([0-9]+)\.([0-9]+)\+quic$]])
+    message(FATAL_ERROR "Version regex did not match.")
+endif()
+set(OPENSSL_VERSION_MAJOR "${CMAKE_MATCH_1}")
+set(OPENSSL_VERSION_MINOR "${CMAKE_MATCH_2}")
+set(OPENSSL_VERSION_FIX "${CMAKE_MATCH_3}")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO quictls/openssl
-    REF c960369e667c985c0e95a7b071e7df0c9360cef6
-    SHA512 c6dd7930058dcbbc448aa354b561c5c63270658cc0b7c9ffdafb4117f416dc9c15826943c0c962470e0b23a60646645fe52144eeada4f9e4c0de711a94c2b28f
-    HEAD_REF openssl-3.0.10+quic
+    REF "openssl-${VERSION}"
+    SHA512 0038467327ca51bdd585ae8eaa058b3f826a982037eb62604fac25d8b7e1b8b245044b06ef480d019dfec60355e68fbbdf15cb217b8388811a0a28de051b77be
+    PATCHES
+        disable-apps.patch
+        disable-install-docs.patch
+        script-prefix.patch
+        windows/install-layout.patch
+        windows/install-pdbs.patch
+        unix/android-cc.patch
+        unix/move-openssldir.patch
+        unix/no-empty-dirs.patch
+        unix/no-static-libs-for-shared.patch
 )
 
-# Option: shared/static
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-set(OPENSSL_SHARED no-shared)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(OPENSSL_SHARED shared)
-endif()
-
-# Option: feature / algorithms from https://github.com/microsoft/msquic
-list(APPEND CONFIGURE_OPTIONS
-    enable-tls1_3 no-makedepend no-dgram no-ssl3 no-psk no-srp
-    no-zlib no-egd no-idea no-rc5 no-rc4 no-afalgeng
-    no-comp no-cms no-ct no-srp no-srtp no-ts no-gost no-dso no-ec2m
-    no-tls1 no-tls1_1 no-tls1_2 no-dtls no-dtls1 no-dtls1_2 no-ssl
-    no-ssl3-method no-tls1-method no-tls1_1-method no-tls1_2-method no-dtls1-method no-dtls1_2-method
-    no-siphash no-whirlpool no-aria no-bf no-blake2 no-sm2 no-sm3 no-sm4 no-camellia no-cast no-md4 no-mdc2 no-ocb no-rc2 no-rmd160 no-scrypt
-    no-weak-ssl-ciphers no-tests
-)
-if(VCPKG_TARGET_IS_UWP)
-    list(APPEND CONFIGURE_OPTIONS no-async)
-endif()
-if(VCPKG_TARGET_IS_WINDOWS)
-    # jom will build in parallel mode, we need /FS for PDB access
-    list(APPEND CONFIGURE_OPTIONS -utf-8 -FS)
-
-elseif(VCPKG_TARGET_IS_IOS)
-    # see https://github.com/microsoft/vcpkg PR 12527
-    # disable that makes linkage error (e.g. require stderr usage)
-    list(APPEND CONFIGURE_OPTIONS no-stdio no-ui no-asm)
-
-endif()
-
-# Option: platform/architecture
-include(${CMAKE_CURRENT_LIST_DIR}/detect_platform.cmake)
-
-# Clean & copy source files for working directories
-file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
-                    ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-)
-get_filename_component(SOURCE_DIR_NAME ${SOURCE_PATH} NAME)
-file(COPY        ${SOURCE_PATH}
-     DESTINATION ${CURRENT_BUILDTREES_DIR})
-file(RENAME      ${CURRENT_BUILDTREES_DIR}/${SOURCE_DIR_NAME}
-                 ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
-file(COPY        ${SOURCE_PATH}
-     DESTINATION ${CURRENT_BUILDTREES_DIR})
-file(RENAME      ${CURRENT_BUILDTREES_DIR}/${SOURCE_DIR_NAME}
-                 ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel)
-
-# see ${SOURCE_PATH}/NOTES-PERL.md
-vcpkg_find_acquire_program(PERL)
-get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-vcpkg_add_to_path(${PERL_EXE_PATH})
-
-if(NOT VCPKG_HOST_IS_WINDOWS)
-    # see ${SOURCE_PATH}/NOTES-UNIX.md
-    find_program(MAKE make)
-    get_filename_component(MAKE_EXE_PATH ${MAKE} DIRECTORY)
-endif()
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    # see ${SOURCE_PATH}/NOTES-WINDOWS.md
-    vcpkg_find_acquire_program(NASM)
-    get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
-    vcpkg_add_to_path(PREPEND ${NASM_EXE_PATH})
-    # note: jom is not for `vcpkg_add_to_path`
-    vcpkg_find_acquire_program(JOM)
-
-elseif(VCPKG_TARGET_IS_ANDROID)
-    # see ${SOURCE_PATH}/NOTES-ANDROID.md
-    if(NOT DEFINED ENV{ANDROID_NDK_ROOT} AND DEFINED ENV{ANDROID_NDK_HOME})
-        message(STATUS "ENV{ANDROID_NDK_ROOT} will be set to $ENV{ANDROID_NDK_HOME}")
-        set(ENV{ANDROID_NDK_ROOT} $ENV{ANDROID_NDK_HOME})
-    endif()
-    if(NOT DEFINED ENV{ANDROID_NDK_ROOT})
-        message(FATAL_ERROR "ENV{ANDROID_NDK_ROOT} is required by ${SOURCE_PATH}/Configurations/15-android.conf")
-    endif()
-    if(VCPKG_HOST_IS_LINUX)
-        set(NDK_HOST_TAG "linux-x86_64")
-    elseif(VCPKG_HOST_IS_OSX)
-        set(NDK_HOST_TAG "darwin-x86_64")
-    elseif(VCPKG_HOST_IS_WINDOWS)
-        set(NDK_HOST_TAG "windows-x86_64")
-    else()
-        message(FATAL_ERROR "Unknown NDK host platform")
-    endif()
-    get_filename_component(NDK_TOOL_PATH $ENV{ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/${NDK_HOST_TAG}/bin ABSOLUTE)
-    vcpkg_add_to_path(PREPEND ${NDK_TOOL_PATH})
-
-endif()
-
-# Configure / Install
-# We need a PERL so can't use `vcpkg_configure_make` directly...
-message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
-vcpkg_execute_required_process(
-    COMMAND ${PERL} Configure ${OPENSSL_SHARED} ${CONFIGURE_OPTIONS}
-        ${PLATFORM}
-        "--prefix=${CURRENT_PACKAGES_DIR}/debug"
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
-    LOGNAME configure-perl-${TARGET_TRIPLET}-dbg
-)
-message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
-vcpkg_execute_required_process(
-    COMMAND ${PERL} Configure ${OPENSSL_SHARED} ${CONFIGURE_OPTIONS}
-        ${PLATFORM}
-        "--prefix=${CURRENT_PACKAGES_DIR}"
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-    LOGNAME configure-perl-${TARGET_TRIPLET}-rel
+vcpkg_list(SET CONFIGURE_OPTIONS
+    enable-static-engine
+    enable-capieng
+    no-ssl3
+    no-weak-ssl-ciphers
+    no-tests
 )
 
-if(VCPKG_TARGET_IS_UWP OR VCPKG_TARGET_IS_WINDOWS)
-    message(STATUS "Building ${TARGET_TRIPLET}-dbg")
-    vcpkg_execute_required_process(
-        COMMAND ${JOM} /K /J ${VCPKG_CONCURRENCY} /F makefile install_dev
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
-        LOGNAME install-${TARGET_TRIPLET}-dbg
-    )
-    message(STATUS "Building ${TARGET_TRIPLET}-rel")
-    vcpkg_execute_required_process(
-        COMMAND ${JOM} /K /J ${VCPKG_CONCURRENCY} /F makefile install_dev
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-        LOGNAME install-${TARGET_TRIPLET}-rel
-    )
-    vcpkg_copy_pdbs()
-
-else()
-    message(STATUS "Building ${TARGET_TRIPLET}-dbg")
-    vcpkg_execute_required_process(
-        COMMAND ${MAKE} -j ${VCPKG_CONCURRENCY} install_dev
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
-        LOGNAME install-${TARGET_TRIPLET}-dbg
-    )
-    message(STATUS "Building ${TARGET_TRIPLET}-rel")
-    vcpkg_execute_required_process(
-        COMMAND ${MAKE} -j ${VCPKG_CONCURRENCY} install_dev
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-        LOGNAME install-${TARGET_TRIPLET}-rel
-    )
-    if(VCPKG_TARGET_IS_ANDROID AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-        # install_dev copies symbolic link. overwrite them with the actual shared objects
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/libcrypto.so
-                     ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/libssl.so
-             DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-        )
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/libcrypto.so
-                     ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/libssl.so
-             DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-        )
-    endif()
-    vcpkg_fixup_pkgconfig()
-
+set(INSTALL_FIPS "")
+if("fips" IN_LIST FEATURES)
+    vcpkg_list(APPEND INSTALL_FIPS install_fips)
+    vcpkg_list(APPEND CONFIGURE_OPTIONS enable-fips)
 endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/libcrypto.a
-                ${CURRENT_PACKAGES_DIR}/debug/lib/libssl.a
-                ${CURRENT_PACKAGES_DIR}/lib/libcrypto.a
-                ${CURRENT_PACKAGES_DIR}/lib/libssl.a
-    )
+    vcpkg_list(APPEND CONFIGURE_OPTIONS shared)
 else()
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin
-                        ${CURRENT_PACKAGES_DIR}/bin
-    )
-    if(VCPKG_TARGET_IS_WINDOWS)
-        file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/ossl_static.pdb
-                    ${CURRENT_PACKAGES_DIR}/lib/ossl_static.pdb
-        )
-    endif()
+    vcpkg_list(APPEND CONFIGURE_OPTIONS no-shared no-module)
 endif()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
-                    ${CURRENT_PACKAGES_DIR}/debug/engines-81.3
-                    ${CURRENT_PACKAGES_DIR}/engines-81.3
-                    ${CURRENT_PACKAGES_DIR}/debug/ossl-modules
-                    ${CURRENT_PACKAGES_DIR}/ossl-modules
-)
 
-file(INSTALL     ${SOURCE_PATH}/LICENSE.txt
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
-)
+if(NOT "tools" IN_LIST FEATURES)
+    vcpkg_list(APPEND CONFIGURE_OPTIONS no-apps)
+endif()
+
+if(DEFINED OPENSSL_USE_NOPINSHARED)
+    vcpkg_list(APPEND CONFIGURE_OPTIONS no-pinshared)
+endif()
+
+if(OPENSSL_NO_AUTOLOAD_CONFIG)
+    vcpkg_list(APPEND CONFIGURE_OPTIONS no-autoload-config)
+endif()
+
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    include("${CMAKE_CURRENT_LIST_DIR}/windows/portfile.cmake")
+    include("${CMAKE_CURRENT_LIST_DIR}/install-pc-files.cmake")
+else()
+    include("${CMAKE_CURRENT_LIST_DIR}/unix/portfile.cmake")
+endif()
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
